bc1a1954a3238f8ff61c1fa82a62ae451eba4520
Denis Cappellin <denis@cappell.in>
2020-05-03 12:19:19 +0200

    Add Linux version of shasum

--- notegen.sh
+++ notegen.sh
@@ -6,7 +6,15 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-FILENAME="`date +%s | shasum -a 256 | head -c 20 ; echo`_$1.md"
+EXTENSION=".md"
+SHA256SUM="shasum -a 256"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SHA256SUM="sha256sum"
+fi
+
+FILENAME="`date +%s | ${SHA256SUM} | head -c 20 ; echo`_$1${EXTENSION}"
+
 touch "${FILENAME}"
 echo -e "# $1" > ${FILENAME}
 echo "File ${FILENAME} created."
